81bcfddc4c6bd7614561929b4d0560caade3e62d
Sebastián <sj@Sebastians-MacBook-Pro.local>
2022-09-13 21:04:06 -0500

    init files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UnderCostruction from "./pages/under-construption/under-construction";
+
+function App() {
+  return (
+    <>
+      <UnderCostruction />
+    </>
+  );
+}
+
+export default App;
